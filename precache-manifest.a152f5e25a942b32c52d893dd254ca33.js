fcf540228dab62916514592a189aee872d137ade
rohitpratap002 <rohitbvoc2023@bpibs.in>
2023-03-12 18:49:59 +0530

    Updates

---
+++ precache-manifest.a152f5e25a942b32c52d893dd254ca33.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "25c063da64c9041c3133",
+    "url": "/static/css/main.cc87bcfe.chunk.css"
+  },
+  {
+    "revision": "25c063da64c9041c3133",
+    "url": "/static/js/main.25c063da.chunk.js"
+  },
+  {
+    "revision": "8bc595e548dc98df2abf",
+    "url": "/static/js/1.8bc595e5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ea29dd5da99d8b5d9268601708be138",
+    "url": "/static/media/project1.6ea29dd5.png"
+  },
+  {
+    "revision": "3a5afe6a5f4873e8c6a7eac63ac7425c",
+    "url": "/static/media/project2.3a5afe6a.png"
+  },
+  {
+    "revision": "7675f9d22e8af35ee950c57486919e47",
+    "url": "/static/media/github.7675f9d2.svg"
+  },
+  {
+    "revision": "5415239d003a9a426c0633277f486935",
+    "url": "/static/media/homeImg.5415239d.svg"
+  },
+  {
+    "revision": "6ce9e0daecb31734ae079af351aa09de",
+    "url": "/static/media/ROHIT-PRATAP-Resume.6ce9e0da.pdf"
+  },
+  {
+    "revision": "213c57b4b65ba454a8149c568bb9d90f",
+    "url": "/static/media/aboutimg.213c57b4.jpg"
+  },
+  {
+    "revision": "cf133bcd731b095b59cc79f79081a39a",
+    "url": "/static/media/html-five.cf133bcd.svg"
+  },
+  {
+    "revision": "b54f637cc47c19316727206493bf2228",
+    "url": "/static/media/css3.b54f637c.svg"
+  },
+  {
+    "revision": "fcec09dccade2e3d3a35af8281e3292e",
+    "url": "/static/media/javascript.fcec09dc.svg"
+  },
+  {
+    "revision": "3b8c151b647464a80ffde18d106dd4bc",
+    "url": "/static/media/reactjs.3b8c151b.svg"
+  },
+  {
+    "revision": "c1a4d2bd27d57f49e7c8f9508d6a2283",
+    "url": "/static/media/python.c1a4d2bd.svg"
+  },
+  {
+    "revision": "9f8535d511378905e1a8ea81fed64f94",
+    "url": "/static/media/nodejs.9f8535d5.svg"
+  },
+  {
+    "revision": "61968812d4162a703995f4aaee9da423",
+    "url": "/static/media/php.61968812.svg"
+  },
+  {
+    "revision": "8203c6230a328efc534d22917117d6a2",
+    "url": "/static/media/calculator.8203c623.jpg"
+  },
+  {
+    "revision": "e2c92287f97f9ecf7c0dbaea7ed1196f",
+    "url": "/static/media/quiz.e2c92287.jpg"
+  },
+  {
+    "revision": "eb31777ffaffe76ce9a6f411f2af2420",
+    "url": "/static/media/salubrita.eb31777f.jpg"
+  },
+  {
+    "revision": "dfc5c0601603d0a2dbc7c1aafe15c6b8",
+    "url": "/static/media/TicTacToe.dfc5c060.jpg"
+  },
+  {
+    "revision": "a4d25017fca6a5c8bc059ef06d4c6686",
+    "url": "/static/media/music.a4d25017.jpg"
+  },
+  {
+    "revision": "4f494d4b13acb2d4d128d346acdcb0ac",
+    "url": "/static/media/timer.4f494d4b.jpg"
+  },
+  {
+    "revision": "b27effc64377058339624b6aeb8ce8f4",
+    "url": "/static/media/drag.b27effc6.jpg"
+  },
+  {
+    "revision": "6388131ce0359bbe2cfdecc9ccb4c761",
+    "url": "/static/media/notes.6388131c.jpg"
+  },
+  {
+    "revision": "0b3ae742857ba459bcfa019e0edcfaa9",
+    "url": "/static/media/mail.0b3ae742.svg"
+  },
+  {
+    "revision": "880e2554ed21307c3aa9ee199216a6ef",
+    "url": "/static/media/instagram.880e2554.svg"
+  },
+  {
+    "revision": "736f5b01625235b05e0570e344e91164",
+    "url": "/static/media/facebook.736f5b01.svg"
+  },
+  {
+    "revision": "b7bad82421513048351d9c36c8d4e379",
+    "url": "/static/media/linkedin.b7bad824.svg"
+  },
+  {
+    "revision": "4538350336a873f55db1518a607860d4",
+    "url": "/static/media/sendIcon.45383503.svg"
+  },
+  {
+    "revision": "15f80155995289121930dcb5f40dbb4c",
+    "url": "/static/media/homeBack.15f80155.png"
+  },
+  {
+    "revision": "839d93ffa238b1cdd9c3556c1249b7b5",
+    "url": "/index.html"
+  }
+];
