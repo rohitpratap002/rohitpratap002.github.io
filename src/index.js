7a875d51030940dcb5a2bb70d5c91eecd5432dd0
rohitpratap002 <rohitbvoc2023@bpibs.in>
2023-03-12 20:03:38 +0530

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import HomeWebsite from './HomeWebsite';
@@ -7,8 +7,8 @@ import HomeWebsite from './HomeWebsite';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <Router>
       <HomeWebsite />
-    </HashRouter>
+    </Router>
   </React.StrictMode>
 );
