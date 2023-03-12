6751f27ca3bba321fd32b31b5eb2ab95e6e697b4
rohitpratap002 <rohitbvoc2023@bpibs.in>
2023-03-12 20:14:03 +0530

    setup gh-pages

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -40,7 +40,7 @@ function Portfolio(){
                     })}
                 </div>
                 <div style={{width:"100%",marginTop:"2rem",display:"flex",flexDirection:"row",justifyContent:"flex-end"}} className='viewContent'>
-                    <a href="/projects" className="btn btnPrimary">View All<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path className='btnArrow' d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z" data-name="Right"/></svg></a>
+                    <a href="/#/projects" className="btn btnPrimary">View All<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25"><path className='btnArrow' d="m17.5 5.999-.707.707 5.293 5.293H1v1h21.086l-5.294 5.295.707.707L24 12.499l-6.5-6.5z" data-name="Right"/></svg></a>
                 </div>
             </div>
         </>
