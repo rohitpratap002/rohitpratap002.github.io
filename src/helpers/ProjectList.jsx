454ed4cdc4197cfe69ce4b0268dde128662c96e1
rohitpratap002 <rohitbvoc2023@bpibs.in>
2023-03-14 22:03:23 +0530

    setup gh-pages

--- src/helpers/ProjectList.jsx
+++ src/helpers/ProjectList.jsx
@@ -13,56 +13,56 @@ const ProjectList=[
         tags:["HTML","CSS","Javascript"],
         description:"This Calculator app is an application developed for android cells phones, tablets and Desktop. It enables the users to perform fundamental mathematical operations such as addition, subtraction, multiplication and division on their Devices.",
         img:project1,
-        link:"https://32ug5o.csb.app/"
+        link:"https://rohitpratap002.github.io/web-calculator/"
     },
     {
         title:"Online Quiz App",
         tags:["ReactJs","HTML","CSS","Javascript"],
         description:"The Web quiz application test your knowledge and understanding in different areas. After each quiz, the user will see the final score and you have the ability to go through the quiz result analysis to see questions that you fail and the correct answers.",
         img:project2,
-        link:"https://allquiz.w3spaces.com/"
+        link:"https://rohitpratap002.github.io/Quiz/"
     },
     {
          title:"Salubrita-Cookie Shop",
         tags:["HTML","CSS","Javascript"],
         description:"Static content is any content that can be delivered to an end user without having to be generated, modified, or processed. The server delivers the same file to each user, making static content one of the simplest and most efficient content types to transmit over the Internet.",
         img:project3,
-        link:"https://7dt8j6.csb.app/"
+        link:"https://rohitpratap002.github.io/salubrita-web/"
     },
     {
         title:"Tic Tac Toe",
         tags:["HTML","CSS","Javascript"],
         description:"The tic-tac-toe game is for two players. One player plays X and the other plays O. The players take turns placing their marks on a grid of three-by-three cells. If a given player gets three marks in a row horizontally, vertically, or diagonally, then that player wins the game.",
         img:project4,
-        link:"https://bz3i5y.csb.app/"
+        link:"https://rohitpratap002.github.io/tic-tac-toe/"
     },
     {
         title:"Music Creation",
         tags:["HTML","CSS","Javascript"],
         description:" A keyboard musical instrument having wire strings that sound when struck by felt-covered hammers operated from a keyboard. Create Your Own Piece of music Using this Web App.",
         img:project5,
-        link:"https://u73eey.csb.app/"
+        link:"https://rohitpratap002.github.io/music-app/"
     },
     {
         title:"Timer App",
         tags:["HTML","CSS","Javascript"],
         description:"As The Name Suggest This Online Web App Let you set Timer For Your any work. When The Given Time is Over It Will Ring to alert You.",
         img:project6,
-        link:"https://y4c73g.csb.app/"
+        link:"https://rohitpratap002.github.io/timer-display/"
     },
     {
         title:"Drag and Draw",
         tags:["HTML","CSS","Javascript"],
         description:"Higly JavaScript based Drawing App try Click and Drag your Cursor on Screen to See Magic.",
         img:project7,
-        link:"https://c9bv6s.csb.app/"
+        link:"https://rohitpratap002.github.io/click-to-draw/"
     },
     {
         title:"Notes App",
         tags:["HTML","CSS","Javascript"],
         description:"The notes app is an application, built for storing information and notes that are important to the person. It also includes creation, storage, sharing, retrieval.",
         img:project8,
-        link:"https://27dhyf.csb.app/"
+        link:"https://rohitpratap002.github.io/notes-app/"
     }
 ]
 
